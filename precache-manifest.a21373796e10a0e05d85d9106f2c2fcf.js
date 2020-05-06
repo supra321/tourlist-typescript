5d54e845fe3c4b0cba85eae3e86860e32ef72598
supra321 <suprativdatta@gmail.com>
2020-05-07 02:41:07 +0530

    Updates

---
+++ precache-manifest.a21373796e10a0e05d85d9106f2c2fcf.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9a384940cc3b4401d8cc525137a9728",
+    "url": "/tourlist-typescript/index.html"
+  },
+  {
+    "revision": "382018e44a5c42a830f2",
+    "url": "/tourlist-typescript/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "4c8d0b00ce63398191ec",
+    "url": "/tourlist-typescript/static/css/main.bc18b43e.chunk.css"
+  },
+  {
+    "revision": "382018e44a5c42a830f2",
+    "url": "/tourlist-typescript/static/js/2.93172476.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tourlist-typescript/static/js/2.93172476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c8d0b00ce63398191ec",
+    "url": "/tourlist-typescript/static/js/main.bcf5dbef.chunk.js"
+  },
+  {
+    "revision": "75fb1bbd8f8f71e0203c",
+    "url": "/tourlist-typescript/static/js/runtime-main.bc14ccdb.js"
+  },
+  {
+    "revision": "66bfce903ee715434be1a3e9c7afcfcb",
+    "url": "/tourlist-typescript/static/media/DARJEELING.66bfce90.jpg"
+  },
+  {
+    "revision": "740fa4ce7ee6c8e432df5b7360423c1d",
+    "url": "/tourlist-typescript/static/media/HYDERABAD.740fa4ce.jpg"
+  },
+  {
+    "revision": "95207719a41e8ced8daf2656338eac6e",
+    "url": "/tourlist-typescript/static/media/Hyderabad.95207719.pdf"
+  },
+  {
+    "revision": "0fc68d8836d71fe0cfc2f2fd048d765f",
+    "url": "/tourlist-typescript/static/media/SHIMLA.0fc68d88.jpg"
+  },
+  {
+    "revision": "f27553cc8952647794c1f88487d060d0",
+    "url": "/tourlist-typescript/static/media/Shimla.f27553cc.pdf"
+  },
+  {
+    "revision": "9e14400cdddd8a3aab6e9f99204c5107",
+    "url": "/tourlist-typescript/static/media/Sravasti.9e14400c.pdf"
+  },
+  {
+    "revision": "e9ff0ec80d1c0e96a47f6497448c8a87",
+    "url": "/tourlist-typescript/static/media/Srinagar.e9ff0ec8.pdf"
+  },
+  {
+    "revision": "56f56c319f80758c6a68ad4be0ca5bc8",
+    "url": "/tourlist-typescript/static/media/futura-pt-medium-589e45b956de4.56f56c31.woff"
+  }
+]);
